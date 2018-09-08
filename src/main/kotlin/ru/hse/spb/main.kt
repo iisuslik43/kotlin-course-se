dd97ee6643921a10451e75914a7b8920b11dfd7d
iisuslik <al.s.fedotov1998@yandex.ru>
2018-09-08 21:42:31 +0300

    First try

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,81 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+
+private class DfsRunner(private val graph: Array<MutableList<Int>>) {
+    private enum class Color { WHITE, GREY, BLACK }
+
+    private val vertexColor = Array(graph.size) { Color.WHITE }
+    val cycle = mutableListOf<Int>()
+
+    fun dfs(vertex: Int, parent: Int = -1): Int {
+        if (vertexColor[vertex] == Color.BLACK) {
+            return -1
+        }
+        if (vertexColor[vertex] == Color.GREY) {
+            return vertex
+        }
+        vertexColor[vertex] = Color.GREY
+        for (neighbor in graph[vertex]) {
+            if (neighbor == parent) {
+                continue
+            }
+            val result = dfs(neighbor, vertex)
+            if (result != -1) {
+                cycle.add(vertex)
+                return if (vertex == result) -1 else result
+            }
+        }
+        vertexColor[vertex] = Color.BLACK
+        return -1
+    }
+}
+
+
+private fun getCycle(graph: Array<MutableList<Int>>): List<Int> {
+    val runner = DfsRunner(graph)
+    runner.dfs(0)
+    return runner.cycle
+}
+
+fun getDistances(graph: Array<MutableList<Int>>): IntArray {
+    val cycle = getCycle(graph)
+    val distance = IntArray(graph.size) { -1 }
+    val queue: Queue<Int> = ArrayDeque<Int>()
+    for (vertex in cycle) {
+        queue.add(vertex)
+        distance[vertex] = 0
+    }
+    while (!queue.isEmpty()) {
+        val vertex = queue.remove()
+        for (neighbor in graph[vertex]) {
+            if (distance[neighbor] == -1) {
+                queue.add(neighbor)
+                distance[neighbor] = distance[vertex] + 1
+            }
+        }
+    }
+    return distance
+}
+
+fun getGraph(edges: List<Pair<Int, Int>>): Array<MutableList<Int>> {
+    val graph = Array<MutableList<Int>>(edges.size) { mutableListOf() }
+    for ((from, to) in edges) {
+        graph[from].add(to)
+        graph[to].add(from)
+    }
+    return graph
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val vertexCount = readLine()!!.split(' ')[0].toInt()
+    val edges = mutableListOf<Pair<Int, Int>>()
+    for (i in 1..vertexCount) {
+        val (from, to) = readLine()!!.split(' ').map { it.toInt() - 1 }
+        edges.add(Pair(from, to))
+    }
+    val graph = getGraph(edges)
+    val distance = getDistances(graph)
+    distance.forEach { print("$it ") }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,25 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testFirst() {
+        val graph = getGraph(listOf(Pair(0, 2), Pair(3, 2), Pair(3, 1), Pair(0, 1)))
+        assertArrayEquals(IntArray(graph.size) { 0 }, getDistances(graph))
+    }
+
+    @Test
+    fun testSecond() {
+        val graph = getGraph(listOf(Pair(0, 1), Pair(2, 3), Pair(5, 3), Pair(1, 2), Pair(0, 2), Pair(2, 4)))
+        assertArrayEquals(intArrayOf(0, 0, 0, 1, 1, 2), getDistances(graph))
+    }
+
+    @Test
+    fun testBigger() {
+        val graph = getGraph(listOf(Pair(0, 1), Pair(1, 2), Pair(2, 3), Pair(3, 5), Pair(3, 4), Pair(4, 5), Pair(5, 6)))
+        assertArrayEquals(intArrayOf(3, 2, 1, 0, 0, 0, 1), getDistances(graph))
     }
 }
